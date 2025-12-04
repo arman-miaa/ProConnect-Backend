3c23c3ddc66c452a2e77e1130d14c0fa1e6b4981
Arman Mia <mdarman365506@gmail.com>
2025-12-04 23:35:08 +0600

    Implement full order flow: create, accept, in-progress, deliver, complete, cancel with active order check

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { UserRoutes } from "../modules/user/user.routes"
 import { AuthRoutes } from "../modules/auth/auth.route"
 import { MessageRoutes } from "../modules/utility-messages/message.route";
 import { ServiceRoutes } from "../modules/service/service.routes";
+import { OrderRoutes } from "../modules/order/order.routes";
 
 export const router = Router()
 const moduleRoutes = [
@@ -17,6 +18,10 @@ const moduleRoutes = [
   {
     path: "/service",
     route: ServiceRoutes,
+  },
+  {
+    path: "/order",
+    route: OrderRoutes,
   },
   {
     path: "/",
